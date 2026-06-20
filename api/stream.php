0c6add37e3b613ea137a3534b652bcc9571fcafd
TECHNO NANO <511ssshsss@gmail.com>
2026-06-21 01:08:04 +0300

    Update stream.php

--- api/stream.php
+++ api/stream.php
@@ -3,16 +3,6 @@
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 
-$matchId = $_GET['match'] ?? '';
-
-if (!$matchId) {
-    echo json_encode([
-        "ok" => false,
-        "error" => "Missing match parameter"
-    ]);
-    exit;
-}
-
 $file = __DIR__ . '/../streams.json';
 
 if (!file_exists($file)) {
@@ -25,9 +15,7 @@ if (!file_exists($file)) {
 
 $data = json_decode(file_get_contents($file), true);
 
-$servers = $data[$matchId] ?? [];
-
 echo json_encode([
     "ok" => true,
-    "servers" => $servers
+    "servers" => $data
 ], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
