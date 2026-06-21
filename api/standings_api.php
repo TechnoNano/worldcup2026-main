29435ccd21e8488354071178b9058a34783f3858
TECHNO NANO <511ssshsss@gmail.com>
2026-06-22 02:08:42 +0300

    Update standings_api.php

--- api/standings_api.php
+++ api/standings_api.php
@@ -1,5 +1,13 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit(0);
+}
+
 require_once __DIR__ . '/../includes/bootstrap.php';
 require_once __DIR__ . '/../includes/helpers.php';
 
