2bdc6252f1a4ea2a2c73a2eb3ea0a695ae1c9727
TECHNO NANO <511ssshsss@gmail.com>
2026-06-22 01:36:53 +0300

    Add files via upload

---
+++ api/standings_api.php
@@ -0,0 +1,14 @@
+<?php
+
+define('WC2026', true);
+
+require_once __DIR__ . '/../includes/bootstrap.php';
+require_once __DIR__ . '/../includes/helpers.php';
+require_once __DIR__ . '/Standings.php';
+
+header('Content-Type: application/json; charset=utf-8');
+
+echo json_encode(
+    Standings::all(),
+    JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT
+);
