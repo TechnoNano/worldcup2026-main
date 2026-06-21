1816e08c9352fdac1370bc5ae888f982aa8e412f
TECHNO NANO <511ssshsss@gmail.com>
2026-06-22 01:47:08 +0300

    Update standings_api.php

--- api/standings_api.php
+++ api/standings_api.php
@@ -2,7 +2,6 @@
 
 require_once __DIR__ . '/../includes/bootstrap.php';
 require_once __DIR__ . '/../includes/helpers.php';
-require_once __DIR__ . '/Standings.php';
 
 header('Content-Type: application/json; charset=utf-8');
 
