03a2857b1eb5d1618c890e052ba696fe2c4a1d4b
TECHNO NANO <511ssshsss@gmail.com>
2026-06-22 16:25:48 +0300

    Add files via upload

--- api/standings_api.php
+++ api/standings_api.php
@@ -1,5 +1,10 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Content-Type: application/json; charset=utf-8");
+
 require_once __DIR__ . '/../includes/bootstrap.php';
 require_once __DIR__ . '/../includes/helpers.php';
 require_once __DIR__ . '/Standings.php';
--- api/streams_api.php
+++ api/streams_api.php
@@ -2,6 +2,7 @@
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Content-Type: application/json; charset=utf-8");
 
 echo file_get_contents(__DIR__ . '/../streams.json');
