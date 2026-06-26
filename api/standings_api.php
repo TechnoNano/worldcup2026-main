79362f7efbacbbda72ce8ffb057fae6e78137ce2
TECHNO NANO <511ssshsss@gmail.com>
2026-06-26 17:08:47 +0300

    Add files via upload

--- api/standings_api.php
+++ api/standings_api.php
@@ -7,6 +7,9 @@ header("Content-Type: application/json; charset=utf-8");
 
 require_once __DIR__ . '/../includes/bootstrap.php';
 require_once __DIR__ . '/../includes/helpers.php';
+require_once __DIR__ . '/Standings.php';
+
+header('Content-Type: application/json; charset=utf-8');
 
 echo json_encode(
     Standings::all(),
