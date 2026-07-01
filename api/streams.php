8b17782b89aecac58a877b4ffa305185dcd6d5e5
TECHNO NANO <511ssshsss@gmail.com>
2026-07-01 23:51:44 +0300

    Update streams.php

--- api/streams.php
+++ api/streams.php
@@ -8,15 +8,15 @@ header("Content-Type: application/json; charset=utf-8");
 echo json_encode([
     [
         "name" => "Server 1",
-        "url" => "https://majed-koora.co/stream.php?channel=majed16majed&file=stream.m3u8"
+        "url" => "https://majed-koora.co/stream.php?channel=chche999&file=stream.m3u8"
     ],
     [
         "name" => "Server 2",
-        "url" => "https://majed-koora.co/stream.php?channel=majed16majed&file=stream.m3u8"
+        "url" => "https://majed-koora.co/stream.php?channel=chche999&file=stream.m3u8"
     ],
     [
         "name" => "Server 3",
-        "url" => "https://euc13.playlist.ttvnw.net/v1/playlist/CtAE0mbzeXhf6fsVlfxNAaqQ4m2GXykqG8Zj5nZj81DEIvFEOpWWzl2mngumSIlphA8Qrmr-bThLlWP-s-7BjI_yLndwXRoc0Ir9x1cD_L0qjQ-Rwj63UdQy6PgxMAuA_-CfGgiHqvQ1W-oju--fN_l3gkTtrpDM14nEHD3A0g9wXXi3S9DEhP5I6gAVYYvOUPxrqkvhV-wT_Vd6rN-e7TrU5grI0wal3eHlXgaFYTWl6JxlpYlDE4-yXwy0ZD3stPlX-2jQcHWVaAkb0YBp_xZNCUgpY6YGpGvk5sPJZU3D2PcwzvqgALn1L0pShXKaG7iKZy6nVI1ONYpOwArYC5R_19k0Kg3-xWqxJ7GgoohnThPwsBLXhGkBCMQWDwfR0USAWW4DsgQ-yYxvkmuFNpPQu6vr8QnT1JqHiR2tMMCknBJeDSU0H0zy8b-K5veyvPGxkYCv1nMEpTxklvn3T1QwmKe5aLP5c03DWou5jMSyo30jhBwx2br6zGswgIufyLDCXO0M0lv6xOzZhIJ19Nv-1PB7QKcLgbMT_-dUik8M8QlzzAyMZHPLcJl5DmrRD75K9StkGYVTNIT6S5Czk8UqhOfZikpsYdrGMqn6D4WOiEE6roRxVsLiYtlFQxBzObjypAmm3oTYvagYaVkWoXV53h9ibUbTr0e8L6Y-7XG7a1gkiHv3_TFQ6fnF1X_y5dSs9gslsdPC2bZOql5etI4Ujfmh0-DJm6EED2uRp7eyow2cGdQ-8x4P6jIFOAHYZyDTrwJXNPiV4jTvPZt5z2dp9xoMsDC4uhXgV5W7LBedIAEqCWV1LXdlc3QtMjC8Dw.m3u8"
+        "url" => "https://majed-koora.co/stream.php?channel=chche999&file=stream.m3u8"
     ],
     [
         "name" => "Server 4",
