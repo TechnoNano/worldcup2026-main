71b3dcfe116d00130b2ad9532a781761db96672e
TECHNO NANO <511ssshsss@gmail.com>
2026-07-06 00:16:10 +0300

    Update streams.php

--- api/streams.php
+++ api/streams.php
@@ -20,6 +20,6 @@ echo json_encode([
     ],
     [
         "name" => "Server 4",
-        "url" => "https://euc12.playlist.ttvnw.net/v1/playlist/CtEEPBw-d6f2t3i2cO2HwJAMaGjTTtdogHrZJoGauWu3ohnSW2CGzPgsTOpt83EhlqSjQxtjCGfZruz6zRQphEPpF7HSi_PaD4ejRd3Mr5_lymBwZwt3EtGIvvfPtYv-U7t58QpeyYyIayJIZO-NnpMyJjZR39LIuiCyzgWmsbaUcNb7rq-gEFpBA4jfV18IIJIbjQdIDKTf9M06RqOibvmh0xDzEVzCxtjHOVgOdYRir_Xt1Ny_tPQGJAbWtnF1eax5DFGJ3_XpLdep-lW2Vp33z834EB_Btlk9L0d9_RQQIR-lTwbpw3n5ni1bIQ_EzTqg8y6BdpVRn3mqj1m_T1huGcljtMqrhKnK3kN_kEq9Gq368S26vAIT2ZXcLU0xTVOuAK0EObedRycBYWqsJX-4ReufDyHRUJrYpLoVfHRzgiNy9esYsEpmBde-6VoSQ2hGiXaX6-luCFPt0lngWGxc_Ixl6gTegkP7OvreLJoBBjN5_Vcqw1fs5z-NkWFXm17W1d0ug8TuDlnucmnvIhzYxJBN4m91GTic1aBGI_vS-zMx8uJLpsVfKRuIN5tlLLB9VSMLpkY5rse-rdTRo0vAnwZ5u1LFIwwj-gvbU3rj8tpFe656DcZZ3n_w7VmPBhnEkZ1Wo1A35RPLthdZN7QMeLjgoSU8Nz3njtcUdR_IAEiBiybiVfouliNPuXPRtpfObiprPXgANZlci8toZyBEMAVX0SfC4liA4rkm41T7BhvFQfbmHjo0jBNJBibQseUcaF3y08tG9rJNxk-fa0MaN-YaDFSqN3OjSrvr8RiXAyABKglldS13ZXN0LTIwvA8.m3u8"
+        "url" => "https://player.twitch.tv/?channel=majed92921&enableExtensions=true&muted=false&parent=twitch.tv&player=popout&quality=auto&volume=0.5"
     ]
 ], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
