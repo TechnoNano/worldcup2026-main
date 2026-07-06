809489feb624bbe6005fae6326024fbcac65491d
TECHNO NANO <511ssshsss@gmail.com>
2026-07-06 16:20:17 +0300

    Update streams.php

--- api/streams.php
+++ api/streams.php
@@ -7,19 +7,19 @@ header("Content-Type: application/json; charset=utf-8");
 
 echo json_encode([
     [
-        "name" => "Server 1",
+        "name" => "Server ",
         "url" => "https://player.twitch.tv/?channel=majed92921&muted=false&parent=majed-koora.co"
     ],
     [
-        "name" => "Server 2",
-        "url" => "https://majed-koora.co/stream.php?channel=majed92921&file=stream.m3u8"
+        "name" => "Server ",
+        "url" => "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_ts/master.m3u8"
     ],
     [
-        "name" => "Server 3",
+        "name" => "Server ",
         "url" => "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
     ],
     [
-        "name" => "Server 4",
-        "url" => "https://player.twitch.tv/?channel=majed92921&enableExtensions=true&muted=false&parent=twitch.tv&player=popout&quality=auto&volume=0.5"
+        "name" => "Server",
+        "url" => "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
     ]
 ], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
