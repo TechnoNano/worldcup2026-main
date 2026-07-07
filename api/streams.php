dbd4458ba184454544f90f6ce4c503ade5e00af9
TECHNO NANO <511ssshsss@gmail.com>
2026-07-07 22:27:50 +0300

    Update streams.php

--- api/streams.php
+++ api/streams.php
@@ -5,45 +5,14 @@ header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: GET, OPTIONS");
 header("Content-Type: application/json; charset=utf-8");
 
-$servers = [
-
+echo json_encode([
     [
-        "id" => 1,
-        "name" => "Server 1",
-        "type" => "twitch",
-        "quality" => "Auto",
-        "default" => true,
-        "url" => "https://player.twitch.tv/?channel=mamajjeedd99&muted=false&parent=majed-koora.com"
+        "name" => "Server ",
+        "url" => "https://player.twitch.tv/?channel=majed92921&muted=false&parent=majed-koora.co"
     ],
-
     [
-        "id" => 2,
-        "name" => "Server2",
-        "type" => "twitch",
-        "quality" => "Auto",
-        "default" => true,
-        "url" => "https://player.twitch.tv/?channel=mamajjeedd99&muted=false&parent=majed-koora.com"
-    
-    ],
+        "name" => "Server ",
+        "url" => "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_ts/master.m3u8"
 
-    [
-        "id" => 3,
-        "name" => "Server 3",
-        "type" => "hls",
-        "quality" => "720p",
-        "default" => false,
-        "url" => "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
-    ],
-
-    [
-        "id" => 4,
-        "name" => "Server 4",
-        "type" => "mp4",
-        "quality" => "HD",
-        "default" => false,
-        "url" => "https://download.samplelib.com/mp4/sample-20s.mp4"
     ]
-
-];
-
-echo json_encode($servers, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
